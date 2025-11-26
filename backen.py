6eed9a04509747918da40bd9139468ad569431d3
CustomCoder4114 <kibetdan4114@gmail.com>
2025-11-26 18:06:23 +0300

    Add files via upload

---
+++ backen.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
+@app.route('/')
+def index():
+     return render_template('learn.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+@app.route('/career')
+def career():
+    return render_template('career.html')
+@app.route('/earn')
+def earn():
+    return render_template('earn.html')
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
